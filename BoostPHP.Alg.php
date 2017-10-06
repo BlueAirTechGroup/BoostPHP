718c23ed8da0d66237d0f7cc41f73b55920e0091
ToiletCommender <cxcyhdyx@sina.com>
2017-10-05 22:12:22 -0400

    Sort Bug Fix
    Fix Bug of quickSort_Native that only returns boolean.
    Change function to quickSort_Slow(), which is useless, to quickSortArrays_ByField()

--- BoostPHP.Alg.php
+++ BoostPHP.Alg.php
@@ -11,37 +11,41 @@ class BoostPHP_AlgorithmClass{
 	* @return array the array after sorting
 	*/
 	function quickSort_Native($array){
-		return sort($array);
+	    $NewArr = $array;
+		sort($NewArr);
+		return $NewArr;
 	}
 	
 	/**
 	* Sort Array by QuickSort Method(Partition)
+	* Array should be in the format of array(array(..., field => int), array(..., field => int));
+	* Example: quickSortArrays_ByField(array("Content"=>"Philosophy No.1", "ListedBy"=>1), array("Content")=>"Math No.2", "ListedBy"=>2)),"ListedBy");
 	* Original Author Britain / Yi_Zhi_Yu (https://segmentfault.com/a/1190000000758502)
 	* Modified by Windy(www.xsyds.cn)
 	* @param array $array array to be sorted
 	* @return array the array after sorting
 	*/
-	function quickSort_Slow($array){
+	function quickSortArrays_ByField($array, $field){
 		//if(!isset($array[1]))
 		if(count($array) <= 1)
 			return $array;
-		$base = $array[0];
+		$base = $array[0][$field];
 		$leftArray = array();
 		$rightArray = array();
 		
 		// 修改部分------------------------
 		for($i=1; $i<count($array); $i++){
-			if($array[$i] > $base)
+			if($array[$i][$field] > $base)
 				$rightArray[] = $array[$i];
 			else // <= base
 				$leftArray[] = $array[$i];
 		}
 		//------------------------------------
 		
-		$leftArray = quickSort($leftArray);
+		$leftArray = quickSort($leftArray,$field);
 		//$leftArray[] = $base;
 		
-		$rightArray = quickSort($rightArray);
+		$rightArray = quickSort($rightArray,$field);
 		return array_merge($leftArray, array($base), $rightArray);
 	}
 }
